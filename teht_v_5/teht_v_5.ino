d5f9f53b1c5461e172a137ace1411045fcce4361
Esa Kesti <esakesti@gmail.com>
2021-08-04 00:57:20 +0100

    Last commit for tonight

--- teht_v_5/teht_v_5.ino
+++ teht_v_5/teht_v_5.ino
@@ -1,25 +1,23 @@
 #include <LiquidCrystal.h>
-LiquidCrystal lcd(12,11,5,4,3,2);
+LiquidCrystal lcd(12,11,5,4,3,1);
 const int switchPin = 6; 
 int switchState = 0; 
 int prevSwitchState = 0; 
 int reply;
 int lampotilaAnturi = A0;
+int lampoNappi = 2;
 void setup(){ 
   pinMode(switchPin, INPUT);
-  /*lcd.begin(16, 2); 
+    lcd.begin(16, 2); 
   lcd.print("Kysy "); 
   lcd.setCursor(0, 1); 
-  lcd.print("Kristallipallolta"); */
+  lcd.print("Kristallipallolta"); 
+    attachInterrupt(digitalPinToInterrupt(lampoNappi), keskeytys, CHANGE);
+
 }
 void loop(){ 
   
-  byte lampo = analogRead(lampotilaAnturi);
-  float lampotila = lampo * (5.0 / 1024.0);          
-      lampotila = (lampotila - 0.5) * 100;
-  lcd.print(lampotila);
-  
-/*  switchState = digitalRead(switchPin);
+switchState = digitalRead(switchPin);
   if(switchState != prevSwitchState){ 
     if(switchState == LOW){ 
       reply = random(8); 
@@ -48,5 +46,12 @@ void loop(){
       } 
     } 
   } 
-  prevSwitchState = switchState;  */
+  prevSwitchState = switchState; 
 } 
+
+void keskeytys(){
+   byte lampo = analogRead(lampotilaAnturi);
+  float lampotila = lampo * (5.0 / 1024.0);          
+      lampotila = (lampotila - 0.5) * 100;
+  lcd.print(lampotila);
+}
